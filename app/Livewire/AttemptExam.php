111b6b56e7b44c73db6d3486a4690eed9fba4087
glenoka <glanokaparisudha@gmail.com>
2025-04-10 14:30:10 +0800

    feat : livewire

---
+++ app/Livewire/AttemptExam.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AttemptExam extends Component
+{
+    public function render()
+    {
+        return view('livewire.attempt-exam');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExamAttemptController;
+use App\Livewire\AttemptExam;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/do-tryout/{id}', [ExamAttemptController::class,'doExam'])->name('do-tryout');
+    // Route::get('/do-tryout/{id}', [ExamAttemptController::class,'doExam'])->name('do-tryout');
+    Route::get('do-exam',AttemptExam::class)->name('do-exam');
 });
