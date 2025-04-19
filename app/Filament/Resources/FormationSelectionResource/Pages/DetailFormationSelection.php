747a3158b9666af6f4f95be582d8e2dd1d24dcac
Glan Oka Parisudha <glanokaparisudha@gmail.com>
2025-04-19 23:17:29 +0800

    feat: approve/reject formation

--- app/Filament/Resources/FormationSelectionResource/Pages/DetailFormationSelection.php
+++ app/Filament/Resources/FormationSelectionResource/Pages/DetailFormationSelection.php
@@ -2,37 +2,117 @@
 
 namespace App\Filament\Resources\FormationSelectionResource\Pages;
 
+use Livewire\Component;
 use Filament\Forms\Form;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\Page;
-use Filament\Forms\Components\Card;
+use Illuminate\Support\Facades\DB;
+use App\Models\Formation_Selection;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use Filament\Forms\Components\FileUpload;
 use App\Filament\Resources\FormationSelectionResource;
-use Filament\Resources\Pages\ViewRecord;
 
-
-class DetailFormationSelection extends ViewRecord
+class DetailFormationSelection extends Page 
 {
     protected static string $resource = FormationSelectionResource::class;
-
     protected static string $view = 'filament.pages.detail-formation-selection';
+   
 
- 
-    
-public function form(Form $form): Form  {
-    return $form
-    ->schema([
-        Section::make('Detail Participant')
-    ->description('Informasi lengkap peserta')
-    ->schema([
-        TextInput::make('participant.name')
-        ->label('Nama Peserta')
-        ->disabled()
-        ->dehydrated()
-        ->default(fn () => $this->record->participant?->name)
+    public $record;
+    public ?array $data = [];
+    public function mount(Formation_Selection $record): void
+    {
+       // $this->record = $record->load('participant');
+        $this->form->fill([
+            'name' => $this->record->participant->name,
+            'nik' => $this->record->participant->nik,
+            'email' => $this->record->participant->email,
+            'tlp' => $this->record->participant->telp,
+            'image' => $this->record->participant->image,
+            'place_of_birth' => $this->record->participant->place_of_birth,
+            'date_of_birth' => $this->record->participant->date_of_birth,
+            'gender' => $this->record->participant->gender,
+            'religion' => $this->record->participant->religion,
+            'address' => $this->record->participant->address,
+            'village' => $this->record->participant->village->name,
+            'formation_name' => $this->record->formation->name,
+        ]);
         
-    ])
-    ]);
-}
+       
+       
+    }
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Form Edit Biodata')
+                ->description(fn ($record) => 'Formasi : ' . $this->record->formation->name)
+                    ->schema([
+                        TextInput::make('name')->required()->readOnly(),
+                        TextInput::make('nik')->required()->readOnly(),
+                        TextInput::make('place_of_birth')->required()->readOnly(),
+                        TextInput::make('date_of_birth')->required()->readOnly(),
+                        TextInput::make('email')->required()->readOnly(),
+                        TextInput::make('tlp')->required()->readOnly(),
+                        FileUpload::make('image')->image()->columnSpanFull(),
+                        TextArea::make('address')->required(),
+                        TextInput::make('village')
+                            ->label('Village')
+                            ->readOnly()
+                            ->default($this->record->participant->village->name),
+                        Select::make('religion')
+                        ->disabled()
+                            ->options([
+                                'islam' => 'Islam',
+                                'kristen' => 'Kristen',
+                                'katolik' => 'Katolik',
+                                'hindu' => 'Hindu',
+                                'budha' => 'Budha',
+                            ])
+                            ->required(), 
+                    ])->columns(2),
+               
+            ])->statePath('data');
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('approve')
+                ->button()
+                ->color('success')
+                ->icon('heroicon-o-check-circle')
+                ->action(function () {
+                    DB::table('formation_selections')
+                    ->where('id', $this->record->id)
+                    ->update(['status' => 'accepted']);
+                    Notification::make()
+                        ->title('Approved Successfully')
+                        ->success()
+                        ->send();
+                })
+                ->visible(fn () => $this->record->status !== 'approved'),
+                
+            Action::make('reject')
+                ->button()
+                ->color('danger')
+                ->icon('heroicon-o-x-circle')
+                ->action(function () {
+                    DB::table('formation_selections')
+                    ->where('id', $this->record->id)
+                    ->update(['status' => 'rejected']);
+                    Notification::make()
+                        ->title('Rejected Successfully')
+                        ->warning()
+                        ->send();
+                })
+                ->visible(fn () => $this->record->status !== 'rejected'),
+        ];
+    }
+    
 
 }
--- resources/views/filament/pages/detail-formation-selection.blade.php
+++ resources/views/filament/pages/detail-formation-selection.blade.php
@@ -1,7 +1,18 @@
 <x-filament::page>
-  
-    {{ $this->form }}
-  
     
-   
+        <!-- Form Section -->
+       
+            {{ $this->form }}
+            <div class="flex justify-end gap-4 pt-6">
+        <button wire:click="reject" type="button" 
+                class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset filament-button-color-danger filament-page-button-action">
+            Reject
+        </button>
+        
+        <button wire:click="approve" type="button"
+                class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset filament-button-color-success filament-page-button-action">
+            Approve
+        </button>
+    </div>
+    
 </x-filament::page>
